803d349e8d1b8f4842150764b207def2e35df612
rondadevil <dalai.ranjan@gmail.com>
2013-06-13 23:56:08 +0530

    New commit

---
+++ Fdffd/src/main/java/com/example/fdffd/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.fdffd;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    Button r, s;
+    TextView t;
+    int counter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        counter = 0;
+        r = (Button) findViewById(R.id.b1);
+        s=(Button) findViewById(R.id.b2);
+        t = (TextView) findViewById(R.id.textView);
+        r.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+
+            }
+        });
+       s.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               counter--;
+           }
+       });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
